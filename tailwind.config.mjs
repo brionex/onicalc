5bd802e128c930776963f16656794a8f6180c323
Jesus Briones <leojesusbriones2@gmail.com>
2023-12-03 15:56:59 -0500

    new page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				"dark-blue": "#0c1222",
-				"dark-navy-blue": "#2d3444",
-				"blue": "#61A4F4"
-			}
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'sky-blue': '#38BDF8',
+        'light-blue': '#E2E8F0',
+        'blue-gray': '#94A3B8',
+        'slate-blue': '#64748B',
+        'dark-slate-blue': '#334155',
+        'dark-gunmetal-blue': '#1E293B',
+        'dark-midnight-blue': '#172032',
+        'dark-ink-blue': '#0B1120',
+      },
+      fontFamily: {
+        quicksand: ['Quicksand', 'system-ui', 'sans-serif'],
+        inter: ['Inter', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
 }
